2828ac89d3feb67a4e1cd01e9847aad5d25bf71f
Alain Gall <alaingall@kl-pt-006.local>
2016-01-24 18:35:47 +0800

    Initial Commit

---
+++ project4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Alain Gall on 24/01/2016.
+//  Copyright © 2016 Alain Gall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
